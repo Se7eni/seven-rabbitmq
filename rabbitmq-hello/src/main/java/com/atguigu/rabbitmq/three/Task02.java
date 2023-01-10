5f1882a9d09cd87550d3dcc98a964c26f2f8cfb6
Se7eni <529958427@qq.com>
2023-01-10 16:46:46 +0800

    RabbitMQ消息应答模式  队列和消息的持久化设置

---
+++ rabbitmq-hello/src/main/java/com/atguigu/rabbitmq/three/Task02.java
@@ -0,0 +1,51 @@
+package com.atguigu.rabbitmq.three;
+
+import com.atguigu.rabbitmq.utils.RabbitMQUtils;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.MessageProperties;
+
+import java.util.Scanner;
+
+/**
+ * 消息在手动应答时是不丢失的，放回队列中重新消费
+ */
+public class Task02 {
+    //队列名称
+    public  static  final String TASK_QUEUE_NAME="ack_queue";
+
+    public static void main(String[] args) throws Exception {
+        Channel channel = RabbitMQUtils.getChannel();
+
+        /**
+         * 生成一个队列
+         * 1：队列名称
+         * 2：队列里面的消息是否持久化（磁盘），默认情况消息存储在内存中；true表示持久化存在磁盘，false表示不持久化存在内存
+         * 3：该队列是否只供一个消费者进行消费，是否进行消息共享，true可以多个消费者消费，false只能一个消费者消费
+         * 4：是否自动删除，最后一个消费者端开连接以后，该队列是否自动删除，true表示自动删除 false不自动删除
+         * 5：其他参数
+         */
+        //队列持久化
+        boolean durable = true;
+        channel.queueDeclare(TASK_QUEUE_NAME,durable,false,false,null);
+        //从控制台接受消息
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+            String message = scanner.next();
+            /**
+             * 发送一个消息
+             * 1；发送到那个交换机
+             * 2；路由的key值是哪个，本次是队列名称
+             * 3；其他参数信息
+             * 4；发送消息的消息体
+             *
+             *  设置生产者发送消息为持久化消息（要求保存到磁盘上），不配置保存在内存中
+             *  MessageProperties.PERSISTENT_TEXT_PLAIN:消息的属性为持久化
+             */
+            channel.basicPublish("",TASK_QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN,message.getBytes("UTF-8"));
+            System.out.println("生产者消息发送完成:"+message);
+
+        }
+
+    }
+
+}
---
+++ rabbitmq-hello/src/main/java/com/atguigu/rabbitmq/three/Work03.java
@@ -0,0 +1,44 @@
+package com.atguigu.rabbitmq.three;
+
+import com.atguigu.rabbitmq.utils.RabbitMQUtils;
+import com.atguigu.rabbitmq.utils.SleepUtils;
+import com.rabbitmq.client.CancelCallback;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+import com.rabbitmq.client.Delivery;
+
+/**
+ * 消息在手动应答时是不丢失，放回队列中重新消费
+ */
+public class Work03 {
+    //队列名称
+    public static  final  String TASK_QUEUE_NAME="ack_queue";
+
+    public static void main(String[] args) throws Exception{
+        Channel channel = RabbitMQUtils.getChannel();
+        System.out.println("C1等待接受消息处理————时间较短");
+
+        //声明接收消息的回调
+        DeliverCallback deliverCallback = ( consumerTag,  message)->{
+            //沉睡1s
+            SleepUtils.sleep(1);
+            System.out.println("接收到的消息："+new String(message.getBody()));
+            //需要手动应答
+            /**
+             * 1.消息的标记 tag
+             * 2.是否批量应答 false:不批量应答信道中的消息，true:批量应答信道中的消息
+             */
+            channel.basicAck(message.getEnvelope().getDeliveryTag(),false);
+        };
+
+        //取消消息的回调
+        CancelCallback CancelCallback =(consumerTag)->{
+            System.out.println("消费者取消消费接口回调");
+        };
+        //采用手动应答
+        boolean autoAck = false;
+        //接受消息
+        channel.basicConsume(TASK_QUEUE_NAME,autoAck,deliverCallback,CancelCallback);
+
+    }
+}
